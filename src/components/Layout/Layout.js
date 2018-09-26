241abb17dd5fa74ea833ee02cb3423e3db8de2c4
Salmane BAH <truegforreal@gmail.com>
2018-09-26 14:17:50 +0200

    Add components for side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: false
+    };
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerOpenHandler = () => {
+        this.setState({showSideDrawer: true});
+    }
+
+    sideDrawerTogglerHandler = () => {
+        this.setState({showSideDrawer: !this.state.showSideDrawer});
+    }
+    
+    render() { 
+        return (
+            <Aux>
+                <Toolbar onDrawerToggle={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} onBackdropClosed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    };
+}
 
 export default Layout;
