2e790b7f325b3db9048cd56c203f842fddc08850
Salmane BAH <truegforreal@gmail.com>
2018-09-26 22:59:36 +0200

    Add more components and reorganize modules

--- src/components/Navigation/SideDrawer/DrawToggle/DrawToggle.js
+++ src/components/Navigation/SideDrawer/DrawToggle/DrawToggle.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './DrawerToggle.css';
 
 const DrawerToggle = (props) => (
-    <div onClick={props.onDrawerToggle}>Menu</div>
+    <div onClick={props.onClick} className={classes.DrawerToggle}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default DrawerToggle;
