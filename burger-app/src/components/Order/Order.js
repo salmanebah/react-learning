9b61580218c92e08e438c5e9a226901cdf6ee09e
Salmane BAH <truegforreal@gmail.com>
2018-10-07 16:56:24 +0200

    Implement Orders and Order components

--- burger-app/src/components/Order/Order.js
+++ burger-app/src/components/Order/Order.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import classes from './Order.css';
 
-const Order = (props) => (
-    <div className={classes.Order}>
-        
-    </div>
-);
+const Order = (props) => {
+    const ingredients = [];
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+    const ingredientOutput = ingredients.map(ingredient => {
+        return <span key={ingredient.name} style={{textTransform: 'capitalize', display: 'inline-block', margin: '0 8px'}}>
+                     {ingredient.name} ({ingredient.amount})
+                </span>;
+    })
+    return (
+        <div className={classes.Order}>
+            <p>{ingredientOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default Order;
